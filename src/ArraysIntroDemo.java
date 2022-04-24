ba646b682e8f2ffb2b430784f3d5cd2dae05e2b6
MyIniqueNam <anton.hometaskrep@gmail.com>
2022-04-24 23:21:09 +0300

    intial commit

---
+++ src/ArraysIntroDemo.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+public class ArraysIntroDemo {
+    public static void main(String[] args) {
+//        char[] symbolsArray = new char[]{'a', 'b', 'c', 'd', 'z', 'a'};
+        //                                0    1    2    3    4    5
+//
+//        System.out.println(symbolsArray[2]);
+//        System.out.println(Arrays.toString(symbolsArray));
+//
+//        int[] numbersArray = {1, 2, 3, 4, 5, 6, -100, 100000};
+//        System.out.println(Arrays.toString(numbersArray));
+//        System.out.println(numbersArray[5]);
+//
+//
+        String[] stringsArray = {"Java", "Go", "C++", "JavaScript"};
+
+//        System.out.println(stringsArray.length);
+
+//
+//        for(int i = 0; i < stringsArray.length; i++) {
+//            System.out.println(stringsArray[i]);
+//        }
+
+
+//        int[][] anotherArray = {{10, 12, 8}, {1, 5}, {4, 7, 8, 10}, {4, 7, 8, 111}};
+//        System.out.println(anotherArray[2] [1] );
+//        System.out.println(anotherArray[1] [1] );
+//        System.out.println(anotherArray[3] [3] );
+
+
+//        for(int i = 0; i < anotherArray.length; i++) {
+//            for (int j = 0; j < anotherArray[i].length; j++) {
+//                System.out.print(anotherArray[i] [j] + " ");
+//            }
+
+
+
+    }
+
+
+}
+
